9a3d4d7a7e1bd5eacafd647a988b29b2c315d7bc
aindriu80 <aindriu80@gmail.com>
2021-02-07 11:05:38 +0000

    Adding Components in Next.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>&#169; Copyright 2021 AMGE List</p>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
+
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi numquam
@@ -45,6 +49,7 @@ export default function Home() {
         perspiciatis natus? Deleniti ratione omnis, ipsam explicabo officiis
         cum.
       </p>
+      <Footer />
     </div>
   );
 }
